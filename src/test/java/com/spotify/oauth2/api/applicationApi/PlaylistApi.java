9edf5a470177344c5ad49289a984fca9da9bc372
Nemanja <ladro_magro@yahoo.com>
2025-04-16 21:03:58 -0400

    First commit

---
+++ src/test/java/com/spotify/oauth2/api/applicationApi/PlaylistApi.java
@@ -0,0 +1,37 @@
+package com.spotify.oauth2.api.applicationApi;
+import com.spotify.oauth2.api.RestResource;
+import com.spotify.oauth2.pojo.Playlist;
+import com.spotify.oauth2.utils.ConfigLoader;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import static com.spotify.oauth2.api.Route.PLAYLISTS;
+import static com.spotify.oauth2.api.Route.USERS;
+import static com.spotify.oauth2.api.TokenManager.getToken;
+
+public class PlaylistApi {
+    @Step
+    public static Response post(Playlist requestPlaylist){
+        return RestResource.post(USERS + "/" + ConfigLoader.getInstance().getUser() + PLAYLISTS, getToken(), requestPlaylist);
+//        return given(getRequestSpec())
+//                .body(requestPlaylist)
+//                .header("Authorization", "Bearer " + access_token)
+//                .when()
+//                .post("/users/31q7mhfvdikbfbhklg7aohgtha3a/playlists")
+//                .then()
+//                .spec(getResponseSpec())
+//                .extract()
+//                .response();
+    }
+    public static Response post(String token, Playlist requestPlaylist){
+        return RestResource.post(USERS + "/" + ConfigLoader.getInstance().getUser() + PLAYLISTS, token, requestPlaylist);
+    }
+
+    public static Response get(String playlist_id){
+        return RestResource.get(PLAYLISTS + "/" + playlist_id, getToken());
+    }
+
+    public static Response update(Playlist requestPlaylist, String playlist_id){
+        return RestResource.update(PLAYLISTS + "/" + playlist_id, getToken(), requestPlaylist);
+    }
+}
---
+++ src/test/java/com/spotify/oauth2/tests/PlaylistTest.java
@@ -0,0 +1,92 @@
+package com.spotify.oauth2.tests;
+import com.spotify.oauth2.api.applicationApi.PlaylistApi;
+import com.spotify.oauth2.pojo.Error;
+import com.spotify.oauth2.pojo.Playlist;
+import com.spotify.oauth2.utils.DataLoader;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.AllureTestNg;
+import io.restassured.response.Response;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@Listeners({AllureTestNg.class})
+@Epic("Spotify OAuth 2.0")
+@Feature("Playlist API")
+public class PlaylistTest {
+
+    @Step
+    public Playlist playlistBuilder(String name, String description, boolean _public){
+        return Playlist.builder()
+                .name(name)
+                .description(description)
+                ._public(_public)
+                .build();
+    }
+
+    @Story("Create a playlist story")
+    @Link("https:/example.org")
+    @Link(name = "allure", type = "mylink")
+    @TmsLink("12345")
+    @Issue("1234567")
+    @Description("Testing if the user can create a playlist.")
+    @Test(description = "Should be able to create a playlist.")
+    public void shouldBeAbleToCreatePlaylist(){
+        Playlist requestPlaylist = playlistBuilder("Elizabeth Playlist", "Elizabeth romantic music", false);
+
+        Response response = PlaylistApi.post(requestPlaylist);
+        assertStatusCode(response.getStatusCode(), 201);
+
+       // Playlist responsePlaylist = response.as(Playlist.class); // deserializing the JSON
+        assertPlaylistEqual(response.as(Playlist.class), requestPlaylist);
+    }
+    @Description("Testing if the user can open the desired playlist.")
+    @Test(description = "Should be able to fetch the playlist.")
+    public void shouldBeAbleToFetchThePlaylist(){
+        Playlist requestPlaylist = playlistBuilder("Elizabeth Playlist", "Elizabeth romantic music", true);
+        Response deserializedResponsePlaylist = PlaylistApi.get(DataLoader.getInstance().getPlaylistId());
+        Playlist serializedResponsePlaylist = deserializedResponsePlaylist.as(Playlist.class);
+        System.out.println("Request playlist public: " + requestPlaylist.get_public());
+        System.out.println("Response playlist public: " + serializedResponsePlaylist.get_public());
+        assertPlaylistEqual(serializedResponsePlaylist, requestPlaylist);
+    }
+    @Description("Testing if the user can update the playlist.")
+    @Test(description = "Should be able to Update playlist.")
+    public void shouldBeAbleToUpdatePlaylist(){
+        Playlist requestPlaylist = playlistBuilder("Elizabeth Playlist", "Elizabeth romantic music", false);
+        Response deserializedResponse = PlaylistApi.update(requestPlaylist, DataLoader.getInstance().getPlaylistId());
+        assertStatusCode(deserializedResponse.getStatusCode(), 200);
+    }
+    @Description("Testing if the user can create a playlist without providing the name.")
+    @Test(description = "Should Not be able to create a playlist without name")
+    public void shouldNotBeAbleToCreateAPlaylistWithoutName(){
+        Playlist requestPlaylist = playlistBuilder("", "Elizabeth romantic music", false);
+        Response response = PlaylistApi.post(requestPlaylist);
+        assertStatusCode(response.statusCode(), 400);
+        assertError(response.as(Error.class), 400,"Missing required field: name");
+    }
+    @Description("Testing if the user can create a playlist with expired token.")
+    @Test(description = "Should not be able to create a playlist with expired token.")
+    public void shouldNotBeAbleToCreateAPlaylistWithExpiredToken(){
+        Playlist requestPlaylist = playlistBuilder("Elizabeth con su carita bonita", "Elizabeth romantic music", false);
+        Response response = PlaylistApi.post("randomValue",requestPlaylist);
+        assertStatusCode(response.statusCode(),401);
+        assertError(response.as(Error.class), 401,"Invalid access token");
+    }
+    @Step
+    public void assertPlaylistEqual(Playlist responsePlaylist, Playlist requestPlaylist){
+        assertThat(responsePlaylist.getName(), equalTo(requestPlaylist.getName()));
+        assertThat(responsePlaylist.getDescription(), equalTo(requestPlaylist.getDescription()));
+        assertThat(responsePlaylist.get_public(), equalTo(requestPlaylist.get_public()));
+    }
+    @Step
+    public void assertStatusCode(int actualStatusCode, int expectedStatusCode){
+        assertThat(actualStatusCode, equalTo(expectedStatusCode));
+    }
+    @Step
+    public void assertError(Error responseErr, int expectedStatusCode, String expectedMsg){
+        assertThat(responseErr.getInnerError().getStatus(), equalTo(expectedStatusCode));
+        assertThat(responseErr.getInnerError().getMessage(), equalTo(expectedMsg));
+    }
+}
